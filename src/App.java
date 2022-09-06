261f1ce775e44eebd5451cfa713ef1e738e979f2
Bryce Fadako <fadakob@chc.edu>
2022-09-06 10:13:21 -0400

    September 6,2022

--- src/App.java
+++ src/App.java
@@ -7,6 +7,12 @@ public class App {
         student Student = new student("Bryce","Fadako","Chc");
         System.out.println(Student.greeting());
 
+        undergradStudent undergrad = new undergradStudent(Student.getFirstName(), Student.getLastName(),
+         Student.getFirstName(), 3.75, 40, "Computer Science");
+        System.out.println(undergrad.greeting());
+        System.out.println(undergrad.creditsRemaining(150));
+        System.out.println(undergrad.isMajorComplete(150));
+
         Integer one = 1;
         Double two = 2.0;
         String twoString = "2.0";
